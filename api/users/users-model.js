35fc05004326977b28101265c1d83f6862cd7800
prodhc <zionywood@gmail.com>
2021-06-15 17:21:52 -0700

    find by id

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -37,6 +37,10 @@ function findBy(filter) {
       }
     ]
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', "password", 'role_name')
+    .where(filter)
 }
 
 function findById(user_id) {
@@ -50,6 +54,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users')
+    .join('roles', 'users.role_id', 'roles.role_id')
+    .select('user_id', 'username', "password", 'role_name')
+    .where('users.user_id', user_id).first()
 }
 
 /**
